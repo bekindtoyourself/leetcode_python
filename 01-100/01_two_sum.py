366c994e10ae7ab0120fb22ac351f8d51619c368
bekindtoyourself <12084895+bekindtoyourself@users.noreply.github.com>
2019-07-16 11:03:43 +0800

    modify 01_two_sum.py

--- 01-100/01_two_sum.py
+++ 01-100/01_two_sum.py
@@ -1,19 +1,6 @@
 # -*- coding: UTF-8 -*-
 
 class Solution:
-    # my ugly code
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for num in nums:
-            i = nums.index(num)
-            for n in nums[i:]:
-                if num + n == target:
-                    return[i, nums.index(n)]
-
     # other guy wonderful code
     def twoSum(self, nums, target):
         """
@@ -28,8 +15,8 @@ class Solution:
             d[num] = i
 
 def main():
-    nums = [2, 7, 11, 15]
-    target = 9
+    nums = [3,3]
+    target = 6
 
     result_list = Solution().twoSum(nums, target)
     print(result_list)
